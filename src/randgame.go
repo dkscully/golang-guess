8ba93f24b5937c7808584fa7c3a94a8123fa7047
Lesley Mitchell <lesley.g.mitchell@gmail.com>
2016-09-09 21:30:48 +0100

    Added a function to test a guess against the generated random number Uses for and switch

--- src/randgame.go
+++ src/randgame.go
@@ -9,7 +9,7 @@ import (
 )
 
 // For testing until I learn how to accept input
-const maxrand = 20
+const maxrand = 2
 
 func setrandno(max int) (a int) {
 	rand.Seed(time.Now().UnixNano())
@@ -17,8 +17,27 @@ func setrandno(max int) (a int) {
 	return a
 }
 
+func checkguess(answer, guess int) (counter int) {
+	counter = 1
+	for guess != answer {
+		counter += 1
+		switch {
+		case guess <= 0:
+			fmt.Println("Your answer is far too small. The number lies between 1 and", maxrand)
+		case guess > maxrand:
+			fmt.Println("Your answer is far too big. The number lies between 1 and", maxrand)
+		case guess < answer:
+			fmt.Println("Too small. Try something bigger.")
+		case guess > answer:
+			fmt.Println("Too big. Try something smaller.")
+		}
+	}
+	return counter
+}
+
 func main() {
 	fmt.Println("The Random Number Guessing Game in Go")
 	randno := setrandno(maxrand)
 	fmt.Println("Random number is", randno)
+	fmt.Println("You guessed in", checkguess(randno, 1), "go(es)")
 }
